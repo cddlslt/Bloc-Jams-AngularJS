872cc88fd99fafda10dd90de63df6cbcbe294a59
cddlslt <oatesmi.74@gmail.com>
2017-09-25 22:18:29 -0500

    commiting to checkout master branch for comparison

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,19 @@
+// (function () {
+//   function SongPlayer() {
+//     var SongPlayer = {};
+//
+//     SongPlayer.play = function(song) {
+//       var currentBuzzObject = new buzz.sound(song.audioUrl, {
+//         formats: ['mp3'],
+//         preload: true
+//       });
+//     }
+//
+//     return SongPlayer;
+//   }
+//
+//   angular
+//     .module('blocJams')
+//     .factoy('SongPlayer', SongPlayer);
+//
+// })();
